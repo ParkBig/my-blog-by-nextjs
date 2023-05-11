9276c58fbda40184d4f312c6262ef3c24d1f955f
github ParkBig <github enfantgu@gmail.com>
2023-05-11 11:55:03 +0900

    갤러그 사진 변경

--- src/lib/my-project-data.tsx
+++ src/lib/my-project-data.tsx
@@ -65,7 +65,7 @@ const data: ProjectContent[] = [
     id: "6",
     title: "Gallagher",
     subTitle: "Personal project / 2022.09 ~ 1weeks",
-    image: ["/assets/my-project/gall1.png", "/assets/my-project/gall2.png", "/assets/my-project/gall3.png"],
+    image: ["/assets/my-project/gall1.png", "/assets/my-project/gall2.png"],
     content: "갤러거 게임입니다. \nCanvas, 생성자 함수, this등을 공부하며 숙련하기 위해 만든 미니게임입니다.",
     subContent: "키보드 조작을 통해 갤러거 게임 가능, 적군이 바닥에 닿으면 게임 종료",
     gitHub: "https://github.com/ParkBig/gallagher-game.git",
